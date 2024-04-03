0463f354ed30d5122db2b41538b0c59f0e060358
Peter Kresanic <personal@kresanic.sk>
2024-04-03 23:45:39 +0200

    # Version 1.4.0 Build 4
    - fixes from 1.3.1 and 1.3.2
    - in April free 6 months to non-Pro users
    - decimals in main price list capacities

---
+++ Fido Building Calcul/Roots/TabView/GivenPromotionalSheet.swift
@@ -0,0 +1,18 @@
+//
+//  GivenPromotionalSheet.swift
+//  Fido Building Calcul
+//
+//  Created by Peter Kresanič on 03/04/2024.
+//
+
+import SwiftUI
+
+struct GivenPromotionalSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GivenPromotionalSheet()
+}
---
+++ Fido Building Calcul/Utilities/Extensions/Date Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Date Extensions.swift
+//  Fido Building Calcul
+//
+//  Created by Peter Kresanič on 03/04/2024.
+//
+
+import Foundation
